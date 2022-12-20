8f746972359a893c415ebc9de65fd1be0798831e
Rashmi <rashmijayasekara00@gmail.com>
2022-12-20 17:13:57 +0530

    Finalize ProjectDAOImpl

--- src/main/java/lk/ijse/dep9/app/dao/custom/impl/ProjectDAOImpl.java
+++ src/main/java/lk/ijse/dep9/app/dao/custom/impl/ProjectDAOImpl.java
@@ -1,13 +1,17 @@
 package lk.ijse.dep9.app.dao.custom.impl;
 
+import lk.ijse.dep9.app.dao.custom.ProjectDAO;
 import lk.ijse.dep9.app.dao.custom.TaskDAO;
+import lk.ijse.dep9.app.entity.Project;
 import lk.ijse.dep9.app.entity.Task;
+import lk.ijse.dep9.app.entity.User;
 
-import java.sql.Connection;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class ProjectDAOImpl implements TaskDAO {
+public class ProjectDAOImpl implements ProjectDAO {
     private final Connection connection;
 
     public ProjectDAOImpl(Connection connection) {
@@ -15,37 +19,90 @@ public class ProjectDAOImpl implements TaskDAO {
     }
 
     @Override
-    public Task save(Task task) {
-        return null;
+    public Project save(Project project) {
+        try {
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO Project(name, username) VALUES (?,?)", Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, project.getName());
+            statement.setString(2, project.getUsername());
+            statement.executeUpdate();
+            ResultSet generatedKeys = statement.getGeneratedKeys();
+            generatedKeys.next();
+            project.setId(generatedKeys.getInt(1));
+            return project;
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
-    public void update(Task task) {
+    public void update(Project project) {
+        try {
+            PreparedStatement statement = connection.prepareStatement("UPDATE Project SET name=? AND username=? WHERE id=?");
+            statement.setString(1,project.getName());
+            statement.setString(2,project.getUsername());
+            statement.setInt(3,project.getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
     @Override
     public void deleteById(Integer pk) {
+        try {
+            PreparedStatement statement = connection.prepareStatement("DELETE FROM Project WHERE id=?");
+            statement.setInt(1,pk);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
     @Override
-    public Optional<Task> findById(Integer pk) {
-        return Optional.empty();
+    public Optional<Project> findById(Integer pk) {
+        try {
+            PreparedStatement statement = connection.prepareStatement("SELECT name,username FROM Project WHERE id=?");
+            statement.setInt(1,pk);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()){
+                return Optional.of(new Project(pk,resultSet.getString("name"),resultSet.getString("username")));
+            }
+            return Optional.empty();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
-    public List<Task> findAll() {
-        return null;
+    public List<Project> findAll() {
+        List<Project> projectList= new ArrayList<>();
+        try {
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM Project");
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()){
+                projectList.add(new Project(resultSet.getInt("id"),resultSet.getString("name"),resultSet.getString("username")));
+            }
+            return projectList;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public long count() {
-        return 0;
+        try {
+            PreparedStatement statement = connection.prepareStatement("SELECT COUNT(id) FROM Project");
+            ResultSet resultSet = statement.executeQuery();
+            return resultSet.getLong(1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public boolean existsById(Integer pk) {
-        return false;
+       return findById(pk).isPresent();
     }
 }
