6bc696715b001cef31789a2536c38bf521a26788
Rashmi <rashmijayasekara00@gmail.com>
2022-12-20 16:44:44 +0530

    Finalize the UserDAOImpl

--- src/main/java/lk/ijse/dep9/app/dao/custom/impl/UserDAOImpl.java
+++ src/main/java/lk/ijse/dep9/app/dao/custom/impl/UserDAOImpl.java
@@ -4,6 +4,10 @@ import lk.ijse.dep9.app.dao.custom.UserDAO;
 import lk.ijse.dep9.app.entity.User;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,37 +20,91 @@ public class UserDAOImpl implements UserDAO {
 
     @Override
     public User save(User user) {
-//        connection.prepareStatement("INSERT INTO User ")
-        return null;
+        try {
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO User(username, full_name, password) VALUES (?,?,?)");
+            statement.setString(1, user.getUsername());
+            statement.setString(2, user.getFullName());
+            statement.setString(3, user.getPassword());
+            statement.executeUpdate();
+            return user;
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+
     }
 
     @Override
     public void update(User user) {
+        try {
+            PreparedStatement statement = connection.prepareStatement("UPDATE User SET full_name=?, password=? WHERE username=?");
+            statement.setString(1,user.getFullName());
+            statement.setString(2,user.getPassword());
+            statement.setString(3,user.getUsername());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
     @Override
-    public void deleteById(String pk) {
+    public void deleteById(String username) {
+        try {
+            PreparedStatement statement = connection.prepareStatement("DELETE FROM User WHERE username=?");
+            statement.setString(1,username);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
     @Override
-    public Optional<User> findById(String pk) {
-        return Optional.empty();
+    public Optional<User> findById(String username) {
+        try {
+            PreparedStatement statement = connection.prepareStatement("SELECT full_name,password FROM User WHERE username=?");
+            statement.setString(1,username);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()){
+                return Optional.of(new User(username, resultSet.getString("password"),resultSet.getString("full_name")));
+            }
+            return Optional.empty();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public List<User> findAll() {
-        return null;
+        List<User> userList=new ArrayList<>();
+        try {
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM User");
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()){
+                userList.add(new User(resultSet.getString("username"),resultSet.getString("password"),resultSet.getString("full_name")));
+            }
+            return userList;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @Override
     public long count() {
-        return 0;
+        try {
+            PreparedStatement statement = connection.prepareStatement("SELECT COUNT(username) FROM User");
+            ResultSet resultSet = statement.executeQuery();
+            resultSet.next();
+            return  resultSet.getLong(1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
-    public boolean existsById(String pk) {
-        return false;
+    public boolean existsById(String username) {
+           return findById(username).isPresent();
     }
 }
