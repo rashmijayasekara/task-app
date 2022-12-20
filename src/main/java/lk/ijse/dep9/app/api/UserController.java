b50874b014d6555ba386099fac5aa9ffde15f85a
Rashmi <rashmijayasekara00@gmail.com>
2022-12-20 18:32:24 +0530

    Add AuthController

--- src/main/java/lk/ijse/dep9/app/api/UserController.java
+++ src/main/java/lk/ijse/dep9/app/api/UserController.java
@@ -1,14 +1,19 @@
 package lk.ijse.dep9.app.api;
 
+import lk.ijse.dep9.app.dto.UserDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
-    @PostMapping
-    public void createNewUserAccount(){
-
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(consumes = "application/json")
+    public void createNewUserAccount(@Valid @RequestBody UserDTO user){
+        System.out.println(user);
     }
     @PatchMapping("/me")
     public void updateUserAccountDetails(){
--- src/main/java/lk/ijse/dep9/app/dto/UserDTO.java
+++ src/main/java/lk/ijse/dep9/app/dto/UserDTO.java
@@ -3,7 +3,12 @@ package lk.ijse.dep9.app.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import java.io.Serializable;
 
 @Data
@@ -11,7 +16,12 @@ import java.io.Serializable;
 @NoArgsConstructor
 
 public class UserDTO implements Serializable {
+    @NotBlank
+    @Pattern(regexp = "^[A-Za-z ]+$")
     private String fullName;
+    @NotBlank
     private String username;
+    @NotEmpty
+    @Length(min = 3)
     private String password;
 }
