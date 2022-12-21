dee0391406814670c6cd99b2e6ae19ecfd1c33c5
Rashmi <rashmijayasekara00@gmail.com>
2022-12-21 14:08:17 +0530

    Refered the connection pool

--- src/main/java/lk/ijse/dep9/app/dao/DAOTypes.java
+++
@@ -1,5 +0,0 @@
-package lk.ijse.dep9.app.dao;
-
-public enum DAOTypes {
-    USER, PROJECT, TASK, QUERY
-}
--- src/main/java/lk/ijse/dep9/app/service/ServiceTypes.java
+++
@@ -1,6 +0,0 @@
-package lk.ijse.dep9.app.service;
-
-public enum ServiceTypes {
-    USER,PROJECT_TASK
-}
-
