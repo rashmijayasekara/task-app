d3cfef6e72e9fe6cdfb3813d56300758f6a466a8
Rashmi <rashmijayasekara00@gmail.com>
2022-12-21 19:14:49 +0530

    Implement the Security Filter

--- src/main/java/lk/ijse/dep9/app/WebAppInitializer.java
+++ src/main/java/lk/ijse/dep9/app/WebAppInitializer.java
@@ -1,7 +1,10 @@
 package lk.ijse.dep9.app;
 
+import lk.ijse.dep9.app.api.filter.SecurityFilter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
 
 @Slf4j
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -22,5 +25,10 @@ public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServlet
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        return new Filter[]{new SecurityFilter()};
     }
 }
--- src/main/java/lk/ijse/dep9/app/api/filter/SecurityFilter.java
+++ src/main/java/lk/ijse/dep9/app/api/filter/SecurityFilter.java
@@ -1,15 +1,46 @@
 package lk.ijse.dep9.app.api.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Base64;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class SecurityFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
-        System.out.println("Incoming Request");
+       if (req.getServletPath().matches("api/v1/users/?") && req.getMethod().equals("POST")){
+           chain.doFilter(req,res);
+       } else if (req.getServletPath().matches("api/v1/auth/.+")) {
+           chain.doFilter(req,res);
+       }else {
+
+           String authorization = req.getHeader("Authorization");
+           if (authorization!=null&& authorization.startsWith("Basic")){
+               String token = authorization.substring("Basic".length() + 1);
+               String credentials = new String(Base64.getDecoder().decode(token));
+               String username = credentials.split(":")[0];
+               String password = credentials.split(":")[1];
+               System.out.println(username+" : "+password);
+
+           }
+           Map<String, Object> errAttributes=new LinkedHashMap<>();
+           errAttributes.put("status", HttpStatus.UNAUTHORIZED.value());
+           errAttributes.put("error",HttpStatus.UNAUTHORIZED.getReasonPhrase());
+           errAttributes.put("message","Invalid Login Credentials");
+           errAttributes.put("timestamp",new Date().toString());
+           ObjectMapper objectMapper = new ObjectMapper();
+           res.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+           res.setContentType("application/json");
+           objectMapper.writeValue(res.getWriter(),errAttributes);
+       }
     }
 }
